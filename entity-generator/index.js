8420089a0f2fc8d0f45b091b96058e85ea72ffd9
yoave11 <yoave11@yahoo.com>
2019-08-07 15:43:31 +0300

    initial entity generator

---
+++ entity-generator/index.js
@@ -0,0 +1,20 @@
+const uuidv1 = require('uuid/v1');
+const kafka = require('kafka-node'),
+    Producer = kafka.Producer,
+    KeyedMessage = kafka.KeyedMessage,
+    client = new kafka.KafkaClient(),
+    producer = new Producer(client),
+    km = new KeyedMessage('key', 'message'),
+    payloads = [
+        { topic: 'entities', messages: {
+
+        }, partition: 0 },
+    ];
+
+producer.on('ready', function () {
+    producer.send(payloads, function (err, data) {
+        console.log(data);
+    });
+});
+ 
+producer.on('error', function (err) {})
