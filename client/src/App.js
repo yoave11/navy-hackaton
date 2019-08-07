0031909ba2c20982c0faee8624e27e70374267e7
oriash93 <oriash93@gmail.com>
2019-08-07 21:48:17 +0300

    add ability to peform http request from view

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import isEmpty from 'lodash.isempty';
 import styled from 'styled-components';
+import axios from 'axios';
 
 import Marker from './components/Marker';
 import GoogleMap from './components/GoogleMap';
 import HAIFA_PORT from './consts/haifa_port';
 
+const url = 'https://httpbin.org/get';
 const Wrapper = styled.section`
   width: 100vw;
   height: 100vh;
@@ -24,6 +26,23 @@ class Main extends Component {
     fetch('places.json')
       .then(response => response.json())
       .then(data => this.setState({ places: data.results }));
+  }
+
+  componentWillMount() {
+    this.getDataAxios()
+  }
+
+  async getDataAxios() {
+    const response =
+      await axios.get(url,
+        {
+          params: {
+            start: '123456789',
+            end: '123456789'
+          }
+        }
+      )
+    console.log(response.data)
   }
 
   render() {
